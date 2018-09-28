81b9ae8e469b4ed3a2a3abaed3aa3a9bda2b193b
Niall Murphy <rinvix@gmail.com>
2018-09-28 11:23:37 +0200

    Install auto suggestions and vundle

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,7 @@ cd $HOME
 git clone https://github.com/nedm2/dotfiles.git .dotfiles
 .dotfiles/install.py
 
-cd $HOME
+touch .$HOME/.z
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
