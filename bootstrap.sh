4928576a5047e3816182db9d2f8fb64a219f1140
Niall Murphy <rinvix@gmail.com>
2019-08-14 12:19:07 +0200

    Cross platform support for zsh autosuggestions

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git clone https://github.com/nedm2/dotfiles.git .dotfiles
 
 touch $HOME/.z
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim -c ":PluginInstall" -c ":qall"
